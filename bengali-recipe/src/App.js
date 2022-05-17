753ca5c63647bea4518351ad684d7d01f067645b
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-05-17 00:47:17 -0400

    Creating an Initial Recipe List catered to Bengali cuisine

--- bengali-recipe/src/App.js
+++ bengali-recipe/src/App.js
@@ -1,25 +1,31 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      dishes: [
+        {name : 'Khichuri',},
+        {name : 'Biriyani',},
+        {name : 'Curry',},
+      ]
+    };
+  }
+  render() {
+    return (
+    <div className='App'> 
+      {
+        this.state.dishes.map((dish) => {
+          return <h1>{dish.name}</h1>;
+        })
+      }
     </div>
   );
+}
 }
 
 export default App;
