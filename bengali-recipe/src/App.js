ef5dcd41b300074014cfd4ac9820351d0264c533
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-05-18 23:25:55 -0400

    Added Recipe lists as a local json file in public folder

--- bengali-recipe/src/App.js
+++ bengali-recipe/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('./data.json')
     .then((response) => response.json())
     .then((users) => this.setState(() => {
       return {dishes: users}
