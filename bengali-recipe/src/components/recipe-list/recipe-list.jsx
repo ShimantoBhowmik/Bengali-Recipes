47e3e90fd69766f40df1e9b99345c6edeba6f578
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-05-21 09:47:28 -0400

    Implementing styling

--- bengali-recipe/src/components/recipe-list/recipe-list.jsx
+++ bengali-recipe/src/components/recipe-list/recipe-list.jsx
@@ -1,13 +1,21 @@
 import { Component } from "react";
 
+import './recipe-list.css';
+
 class RecipeList extends Component{
     render() {
         const {dishes} = this.props;
         return (
-        <div> 
-            {dishes.map(dish =>(
-                <h1 key = {dish.id}>{dish.name}</h1>
-            ) )}
+        <div className="recipe-list"> 
+            {dishes.map(dish =>{
+                const{ name, email, id} = dish;
+                return(
+                <div className= 'recipe-container' key = {id}>
+                    <img alt= {`Recipe ${name}`} src= {`recipe-images/${id}.jpg`} style = {{width: 180,height: 180}}/>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
+        )} )}
         </div>
         )
     }
